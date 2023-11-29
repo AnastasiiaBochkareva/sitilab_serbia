28c5114dea025e378e80fe0c6415a82103258236
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2023-11-29 23:22:37 +0400

    add header js

--- src/mixins/Header/Header.js
+++ src/mixins/Header/Header.js
@@ -1,12 +1,21 @@
-document.addEventListener('click', (event) => {
-  const { target } = event;
-  if (target.closest('.header__lang_choosing ')) {
-    const parent = target.closest('.header__lang ');
-    console.log(parent);
-    if (parent.classList.contains('active')) {
-      parent.classList.remove('active');
-    } else {
-      parent.classList.add('active');
+function showListLanguages() {
+  document.addEventListener('click', (event) => {
+    const { target } = event;
+    const button = target.closest('.header__lang');
+
+    if (button) {
+      if (button.classList.contains('active')) {
+        button.classList.remove('active');
+      } else {
+        button.classList.add('active');
+      }
     }
-  }
-});
+    const close = document.querySelector('.header__lang');
+    const modal = target.closest('.header__lang_choosing');
+    console.log(modal);
+    if (close.classList.contains('active') && !button && !modal) {
+      close.classList.remove('active');
+    }
+  });
+}
+showListLanguages();
