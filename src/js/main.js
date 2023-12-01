4de7f65874cce88abb07582f2ad54eec7e3e8039
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2023-12-01 18:25:46 +0400

    add new blocks

--- src/js/main.js
+++ src/js/main.js
@@ -55,8 +55,8 @@ const mainServices = new Swiper('.main-services__slider', {
   slidesPerView: '3',
   spaceBetween: 20,
   navigation: {
-    prevEl: '.main-services__navigation_prev',
-    nextEl: '.main-services__navigation_next',
+    prevEl: '.slider-navigation_prev',
+    nextEl: '.slider-navigation_next',
   },
   observer: true,
   observeParents: true,
@@ -64,23 +64,88 @@ const mainServices = new Swiper('.main-services__slider', {
   grabCursor: true,
   slideToClickedSlide: true,
   pagination: {
-    el: '.main-services__pagination',
+    el: '.slider-pagination',
     type: 'bullets',
   },
   breakpoints: {
-    1440: {
+    1260: {
       slidesPerView: '3',
       spaceBetween: 20,
     },
-    900: {
-      slidesPerView: '3',
+    1001: {
+      slidesPerView: '2',
+    },
+    800: {
+      slidesPerView: '2.5',
+      spaceBetween: 10,
+    },
+    700: {
+      slidesPerView: '2',
+    },
+    600: {
+      slidesPerView: '1.8',
     },
     500: {
-      slidesPerView: '2',
+      slidesPerView: '1.6',
+    },
+    430: {
+      slidesPerView: '1.4',
     },
     300: {
       slidesPerView: '1',
-      spaceBetween: 10,
     },
   },
+});
+
+// if (document.documentElement.clientWidth < 800) {
+//   const mainAbout = new Swiper('.main-about__slider', {
+//     modules: [Pagination],
+//     slidesPerView: '4',
+//     spaceBetween: 20,
+//     observer: true,
+//     observeParents: true,
+//     observeSlideChildren: true,
+//     grabCursor: true,
+//     slideToClickedSlide: true,
+//     pagination: {
+//       el: '.slider-pagination',
+//       type: 'bullets',
+//     },
+//   });
+// }
+
+let mainAbout = null;
+
+function initSwiper() {
+  if (document.documentElement.clientWidth < 800) {
+    if (!mainAbout) {
+      mainAbout = new Swiper('.main-about__slider', {
+        modules: [Pagination],
+        slidesPerView: '4',
+        spaceBetween: 20,
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true,
+        grabCursor: true,
+        slideToClickedSlide: true,
+        pagination: {
+          el: '.slider-pagination',
+          type: 'bullets',
+        },
+      });
+    }
+  } else {
+    if (mainAbout) {
+      mainAbout.destroy();
+      mainAbout = null;
+    }
+  }
+}
+
+// Вызываем функцию при загрузке страницы
+initSwiper();
+
+// Добавляем обработчик события resize для проверки изменения размера окна
+window.addEventListener('resize', () => {
+  initSwiper();
 });
